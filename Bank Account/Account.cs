0d29ef736f4d0e5fe52c7473e8409d62a9c3bfbc
Jennifer Krejsa <jlkrejsa@gmail.com>
2017-01-27 16:52:46 -0500

    first commit

---
+++ Bank Account/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Account
+{
+    class Account
+    {
+        //fields
+        //View Client Information - user profile
+        //View Account Balance
+        //Checking Account Balance
+        //Reserve Account Balance
+        //Savings Account Balance
+        //Deposit Funds
+        //Withdraw Funds
+        //Exit
+
+        private string clientName;
+        //float checkingBalance;
+        //float reserveBalance;
+        //float savingsBalance;
+        
+
+        //properties
+        public string FirstName
+        {
+            get { return this.clientName; }
+            set { this.clientName = "Gillian Kate"; }
+        }
+
+
+      
+
+
+        
+
+
+
+
+
+
+        }
+}
